b527bc519439ca1b81bbaa67e051f8edd2185249
enqi-123 <252488W@mymail.nyp.edu.sg>
2026-08-06 12:53:39 +0800

    test(uc-005): complete backend tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,8 +2,13 @@ const path = require("path");
 
 module.exports = {
   testEnvironment: "node",
+
   roots: [
     path.resolve(__dirname, "tests"),
     path.resolve(__dirname, "../tests"),
   ],
+
+  moduleNameMapper: {
+    "^supertest$": require.resolve("supertest"),
+  },
 };
